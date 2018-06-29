ccfc3e8f71cef52a834ab8c81662a5a6df532736
Carsten Barylski <carsten.barylski@posteo.de>
2018-06-29 06:47:08 +0200

    4.5 Partial added, Carousel added

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,10 +1,10 @@
 class SimplePagesController < ApplicationController
 
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(3)
   end
 
   def index
   end
-  
+
 end
